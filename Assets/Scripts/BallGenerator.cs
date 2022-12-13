5baaaa2ea06dc33e1da3786f8bd0e69a3fbecf33
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-13 12:03:20 +0900

    Ball image　change

--- Assets/Scripts/BallGenerator.cs
+++ Assets/Scripts/BallGenerator.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class BallGenerator : MonoBehaviour
 {
     // Ballを生成
-    // ・BallのPrefabを用意
-    // ・Instantiateしてやる
     [SerializeField] GameObject ballPrefab = default;
+    // 画像の設定
+    // ・設定する画像を用意
+    [SerializeField] Sprite[] ballSprites = default;
 
     private void Start()
     {
         StartCoroutine(Spawns(40));
+        Debug.Log(ballSprites.Length);
     }
 
     public IEnumerator Spawns(int count)
     {
         for (int i = 0; i < count; i++)
         {
-            Vector2 pos = new Vector2(Random.Range(-0.2f,0.2f),8f);
-            Instantiate(ballPrefab, pos, Quaternion.identity);
+            Vector2 pos = new Vector2(Random.Range(-0.2f, 0.2f), 8f);
+            GameObject ball = Instantiate(ballPrefab, pos, Quaternion.identity);
+            
+            // 画像の設定
+            int ballID = Random.Range(0, ballSprites.Length); // 0,1,2,3,4
+            
+            ball.GetComponent<SpriteRenderer>().sprite = ballSprites[ballID];
             yield return new WaitForSeconds(0.04f);
         }
     }
